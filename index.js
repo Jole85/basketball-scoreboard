3774dec62107081b5cf29f7fb777e42aff026797
Jole85 <jolepavlovic@outlook.com>
2025-06-30 03:38:10 +0200

    javascript:;

---
+++ index.js
@@ -0,0 +1,12 @@
+let homeScoreEl = document.getElementById("score-home");
+let guestScoreEl = document.getElementById("score-guest");
+
+let homeScoreBtnOne = document.getElementById("home-score-btn-1");
+let homeScoreBtnTwo = document.getElementById("home-score-btn-2");
+let homeScoreBtnThree = document.getElementById("home-score-btn-3");
+let resetHomeScore = document.getElementById("home-restart-score-btn");
+
+let guestScoreBtnOne = document.getElementById("guest-score-btn-1");
+let guestScoreBtnTwo = document.getElementById("guest-score-btn-2");
+let guestScoreBtnThree = document.getElementById("guest-score-btn-3");
+let resetGuestScore = document.getElementById("guest-restart-score-btn");
